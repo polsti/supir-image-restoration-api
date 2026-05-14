e7802ad1ee1e1614869057e54d084eeedc3bf3c2
Polina Stina <polinastina@Polinas-MacBook-Pro.local>
2026-05-14 09:45:00 +0300

    fixed definition

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.responses import FileResponse
 
-from app.utils import save_uploaded_image, validate_image_file
+from app.utils import save_uploaded_image, validate_image_size
 from app.supir_service import supir_service
 
 
